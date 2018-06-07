6ca0007713347fac4ccab6c5d1f4ab287580c692
sycho <sycho0311@gmail.com>
2018-06-07 17:49:47 +0900

    create new project for helloAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';import { HttpModule } from '@angular/http';
+
+import { DataService } from './data.service';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { SearchCustomerComponent } from './search-customer/search-customer.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    CustomerDetailsComponent,
+    CreateCustomerComponent,
+    SearchCustomerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
